f0b5483cc55fe885e46c723af546543c52e06b09
minnunae <69183312+minnunae@users.noreply.github.com>
2021-01-21 12:04:24 +0700

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<from action = "welcome.php" method = "post">
+Name: <input type="text" name = "name"><br>
+E-mail: <input type="text" name = "email"><br>
+<input type = "submit">
+</from>
+</body>
+</html>
